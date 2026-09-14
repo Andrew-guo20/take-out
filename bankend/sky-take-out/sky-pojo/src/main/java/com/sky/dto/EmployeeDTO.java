bc63287433194ed36685f761e818e4910bb42573
余世国 <yushiguo@mixlinker.com>
2026-09-14 17:28:29 +0800

    处理登录账号bug

--- bankend/sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ bankend/sky-take-out/sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -3,6 +3,8 @@ package com.sky.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+// Lombok，一个Java类库，提供了一组注解，简化POJ0实体类开发
+// @Data为当前实体类在编译期设置对应的get/set方法，无参/无参构造方法，toString方法，hashCode方法，equals方法等
 
 @Data
 public class EmployeeDTO implements Serializable {
